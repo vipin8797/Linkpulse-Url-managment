bb87da842affabef50ac870c92428b2c037b274c
vipin yadav <cy7795151@gmail.com>
2025-03-25 20:19:03 +0530

    now ready.

--- routes/linkRoutes.js
+++ routes/linkRoutes.js
@@ -59,7 +59,7 @@ router.delete("/yourLinks/:id",wrapAsync(deleteQr));
 
 
 //get for Summary Analytics
-router.get("/summaryAnalytics",
+router.get("/summaryAnalytics",isLoggedIn,
     wrapAsync(getSummaryasync));
 
 
