c385f75313ae35dbd33baef921a3321ad77ae735
vipin8797 <123484468+vipin8797@users.noreply.github.com>
2025-03-31 23:17:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -331,33 +331,33 @@ app.get("/logout", (req, res) => {
 //     res.redirect(shortUrl.originalUrl);
 // });
 app.get("/:subdomain/:shortCode", trackAnalytics, async (req, res, next) => {
-    console.log("🔹 Incoming Request:", req.params);
+    // console.log("🔹 Incoming Request:", req.params);
+   console.log("getting requ for shorted url");
+    // let { subdomain, shortCode } = req.params;
+    // let fullDomain = `${subdomain}.linkpulse.fun`;  // Always attaching .linkpulse.fun
 
-    let { subdomain, shortCode } = req.params;
-    let fullDomain = `${subdomain}.linkpulse.fun`;  // Always attaching .linkpulse.fun
+    // // 🔍 Find the Short URL in MongoDB
+    // const shortUrl = await ShortUrl.findOne({ 
+    //     shortUrl: `https://${fullDomain}/${shortCode}` 
+    // });
 
-    // 🔍 Find the Short URL in MongoDB
-    const shortUrl = await ShortUrl.findOne({ 
-        shortUrl: `https://${fullDomain}/${shortCode}` 
-    });
+    // console.log(shortUrl);
 
-    console.log(shortUrl);
+    // if (!shortUrl) {
+    //     req.flash('error', message = "URL you are Searching For is not found!");
+    //     return res.render('index/404.ejs', { message });
+    // }
 
-    if (!shortUrl) {
-        req.flash('error', message = "URL you are Searching For is not found!");
-        return res.render('index/404.ejs', { message });
-    }
+    // if (!shortUrl.isActive) {
+    //     return next(new ExpressError(404, "❌ URL Expired"));
+    // }
 
-    if (!shortUrl.isActive) {
-        return next(new ExpressError(404, "❌ URL Expired"));
-    }
+    // // Updating lastAccessed of shortUrl
+    // shortUrl.lastAccessedAt = Date.now();
+    // await shortUrl.save(); // Save update in DB
 
-    // Updating lastAccessed of shortUrl
-    shortUrl.lastAccessedAt = Date.now();
-    await shortUrl.save(); // Save update in DB
-
-    console.log("✅ Redirecting to:", shortUrl.originalUrl);
-    res.redirect(shortUrl.originalUrl);
+    // console.log("✅ Redirecting to:", shortUrl.originalUrl);
+    // res.redirect(shortUrl.originalUrl);
 });
 
 
