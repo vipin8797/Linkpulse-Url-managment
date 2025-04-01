504c29daaddf8471a840f26151b15c119d78bf90
vipin8797 <123484468+vipin8797@users.noreply.github.com>
2025-04-01 18:56:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ app.use((req, res, next) => {
 
 
 
-app.get("/:shortCode", trackAnalytics, async (req, res, next) => {
+app.get("/:shortCode", async (req, res, next) => {
     // console.log("🔹 Incoming Request:", req.params);
  console.log("got from first");
    
